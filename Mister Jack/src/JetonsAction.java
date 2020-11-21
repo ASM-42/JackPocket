bba7ffc7ffc44323ab32d2db9eddee436972428f
ASM-42 <74409938+ASM-42@users.noreply.github.com>
2020-11-21 20:40:08 +0100

    Modifications Solene 20/11

--- Mister Jack/src/JetonsAction.java
+++ Mister Jack/src/JetonsAction.java
@@ -1,7 +1,74 @@
-public class JetonsAction {
-    public String typeAction;
 
-    public JetonsAction(String typeAction) {
-        this.typeAction = typeAction;
+public class JetonsAction  {
+    public String typeAction1;
+    public String typeAction2;
+    public String [] jetons;
+
+    public JetonsAction(String typeAction, String typeAction2) {
+        this.typeAction1 = typeAction1;
+        this.typeAction2 = typeAction2;
     }
+
+    public void piocherAlibi (){
+
+    }
+
+    public void bougerSherlock (int[][] position ) {
+        position dective + 1 ou 3
+    }
+
+    public void bougerTobby (position detective ) {
+        position detective +1 ou 3
+    }
+
+    public void bougerWatson (position detective) {
+        position detective +1 ou 3
+    }
+
+    public void rotationDistrict () {
+
+    }
+
+    public void echangeDistrict() {
+
+    }
+
+    public void joker () {
+
+        String reponse1;
+        String reponse2;
+        Scanner scanner = new Scanner (System.int);
+
+        if (player = enqueteur)
+
+            System.out.println ("Quel detective voulez vous déplacer ?");
+            reponse1 = scanner.next();
+
+            if (reponse1.equals('Watson'))
+            position detective + 1
+            if (reponse1.equals('Sherlock'))
+            position detective + 1
+            if (reponse1.equals('Tobby'))
+            position detective + 1
+
+        if (player = MrJack)
+            System.out.println ("Voulez vous déplacer un detective, si non tapez non si oui tapez le nom du detective à déplacer ?");
+            reponse1 = scanner.next();
+            System.out.println ("Voulez vous déplacer le détective d'un seul espace (tapez 1) ou de deux espaces (tapez 2) ?");
+            reponse2 = scanner.next();
+
+            if (reponse1.equals('Watson'))
+                position detective + 1
+            if (reponse1.equals('Sherlock'))
+                position detective + 1
+            if (reponse1.equals('Tobby'))
+                position detective + 1
+            if (reponse1.equals('non'))
+                position detective = position detective;
+
+
+    }
+
+
 }
+

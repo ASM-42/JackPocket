c00a4b2d1d00526e85a07d435f84ed63dd35fd94
ASM-42 <74409938+ASM-42@users.noreply.github.com>
2020-12-17 11:40:19 +0100

    Amandine

--- Mister Jack/src/PersonnageDetective.java
+++ Mister Jack/src/PersonnageDetective.java
@@ -6,4 +6,54 @@ public class PersonnageDetective extends Detective {
         super(name, role, nbSabliers);
         this.position = position;
     }
+    int Vision(){
+        Visibilite=0;
+
+        if (Position=Dx[0]) {
+            if (Dx[0][0]=1) {
+                Visibilite=0;
+            }
+            if (Dx[0][0]=0) {
+                while (Dx[0][0]!= 1) {
+                    Dx= Dx+1;       //Vers la droite
+                    Visibilite=Dx;
+                }
+            }
+
+        }
+        if (Position=Dx[1])  {
+            if (Dx[1][0]=1){
+                Visibilite=0;
+            }
+            if (Dx[1][0]=0){
+                while (Dx[1][0]!= 1) {
+                    Dx= Dx+3; //Vers le bas
+                    Visibilite=Dx;
+                }
+
+            }
+        }
+        if (Position=Dx[2]) {
+            if (Dx[2][0]=1){
+                Visibilite=0;
+            }
+            if (Dx[2][0]=0) {
+                while (Dx[2][0]!= 1) {
+                    Dx= Dx-1;       //Vers la gauche
+                    Visibilite=Dx;
+                }
+            }
+        }
+        if (Position=Dx[3]) {
+            if (Dx[3][0]=1){
+                Visibilite=0;
+            }
+            if (Dx[3][0]=0) {
+                while (Dx[3][0]!= 1) {
+                    Dx= Dx-3;       //Vers le haut
+                    Visibilite=Dx;
+                }
+            }
+        }
+    }
 }

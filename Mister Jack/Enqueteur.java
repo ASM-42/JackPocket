43851d6f6479ddc7f08e4df3e8bb411e68567886
Amandine-java <74062260+Amandine-java@users.noreply.github.com>
2021-01-12 15:00:58 +0100

    Update and rename Mister Jack/src/Detective.java to Mister Jack/Enqueteur.java

--- Mister Jack/Enqueteur.java
+++ Mister Jack/Enqueteur.java
@@ -1,10 +1,10 @@
-public class Detective extends Player{
+public class Enqueteur extends Player{
     public int nbSabliers;
     public PersonnageDetective Waston;
     public PersonnageDetective Tobby;
     public PersonnageDetective Sherlock;
 
-    public Detective(String name, int role, int nbSabliers) {
+    public Enqueteur(String name, int role, int nbSabliers) {
         super(name, role);
         this.nbSabliers = nbSabliers;
     }

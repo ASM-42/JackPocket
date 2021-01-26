ad2ffaa23d96295495b1f949e9b5f815d3ae7e34
ASM <s.touazi@eleves-alsacienne.org>
2021-01-27 00:06:09 +0100

    Graphique

--- src/main/java/com/isep/mrjack/MonInteraction.java
+++ src/main/java/com/isep/mrjack/MonInteraction.java
@@ -1,24 +1,51 @@
 package com.isep.mrjack;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
-public class MonInteraction extends JFrame {
+public class MonInteraction extends JFrame
+{
+    JPanel panel = new JPanel();
+    JLabel label = new JLabel();
+    JTextArea texte = new JTextArea();
+    JTextField zoneEcriture = new JTextField(30);
+    JButton jb = new JButton("Enter");
 
+    public MonInteraction()
+    {
+        setTitle("Interaction");
+        setVisible(true);
+        setSize(800, 500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        texte.setText("Bonjour et bienvenue dans Mister Jack Pocket \n");
+        texte.append("Qui êtes vous?");
+        texte.setBounds(40,30,200,40);
+        panel.add(texte);
+        panel.add(zoneEcriture);
+        zoneEcriture.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String input = zoneEcriture.getText();
+            }});
+        panel.add(jb);
+        jb.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                String input = zoneEcriture.getText();
+            }
+        });
+        zoneEcriture.setBounds(10, 10, 10, 3);
+        panel.add(label);
+        add(panel);
 
-    public MonInteraction() { super("Fenêtre d'interaction"); }
-
-    public void initInteraction() {
-        Interaction pan = new Interaction();
-        this.setContentPane(pan);
     }
 
-
-/*
-    public void messageAccueil() {
-        JPanel textPanel = new JPanel();
-        JButton message = new JButton ("coucou");
-        textPanel.add(message);
+    public static void main(String[] args)
+    {
+        MonInteraction t = new MonInteraction();
 
     }
-    */
+
 }
